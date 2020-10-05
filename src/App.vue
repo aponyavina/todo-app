e295b4a02cc473dcbfe51f09755ef8bcf03d90f0
Anna Ponyavina <ponyavina1997@mail.ru>
2020-10-05 15:31:52 +0300

    create: new components

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>Todo application</h1>
+    <TodoList
+            v-bind:todos="todos"
+            @remove-todo="removeTodo"
+    />
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [
+        {id: 1, title: 'Купить хлеб', completed: false},
+        {id: 1, title: 'Купить масло', completed: false},
+        {id: 1, title: 'Купить пиво', completed: false}
+      ]
+    }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    }
+  },
+  components: {
+    TodoList
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
